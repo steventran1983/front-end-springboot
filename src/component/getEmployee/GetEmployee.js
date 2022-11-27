9f59e8d01f6b02964dc31f1dac4ff57e213107fb
ttran65 <thang.tran65@t-mobile.com>
2022-11-26 23:07:11 -0600

    change test

--- src/component/getEmployee/GetEmployee.js
+++ src/component/getEmployee/GetEmployee.js
@@ -1,106 +1,108 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import toast from "react-hot-toast";
-import AddEmployee from "../addEmployee/AddEmployee";
 import "./getemployee.css";
 import GetOneEmployee from "./GetOneEmployee";
 import ModifyEmployee from "../modifyEmployee/ModifyEmployee";
+
 const GetEmployee = () => {
-  const [load, setLoad] = useState(false);
-  const [toggle, setToggle] = useState(true);
-  const [employees, setEmployees] = useState([]);
-  const [modify,setModify] = useState(false)
-  const [update,setUpdate] = useState(false)
-  const [employeeId,setEmployeeId] = useState("")
-  const handleModify = (empId) => {
-    setModify(true)
-    setEmployeeId(empId)
-  }
-
-  const handleGetEmployees = async () => {
-    try {
-      const res = await axios.get("http://localhost:8080/employees/all");
-      setEmployees(res.data);
-      toast.success("Get Employees Completed");
-    } catch (error) {
-      toast.error("Cannot coonect DB");
+    const [load, setLoad] = useState(false);
+    const [toggle, setToggle] = useState(true);
+    const [employees, setEmployees] = useState([]);
+    const [modify, setModify] = useState(false)
+    const [update, setUpdate] = useState(false)
+    const [employeeId, setEmployeeId] = useState("")
+    const handleModify = (empId) => {
+        setModify(true)
+        setEmployeeId(empId)
     }
-    setToggle(true);
-  };
 
-  const handleDelete = async (empId) => {
-    console.log(empId);
-    try {
-      await axios.delete(`http://localhost:8080/employees/${empId}`);
-      toast.success("Employee Deleted");
-      setLoad(!load);
-    } catch (error) {
-      toast.error("Employee cannot Deleted");
-    }
-  };
+    const handleGetEmployees = async () => {
+        try {
+            const res = await axios.get("https://springboot-homework.herokuapp.com/employees/all");
+            setEmployees(res.data);
+            toast.success("Get Employees Completed");
+        } catch (error) {
+            toast.error("Cannot coonect DB");
+        }
+        setToggle(true);
+    };
 
-  useEffect( () => {
-    handleGetEmployees();
-  }, [load,update]);
+    const handleDelete = async (empId) => {
+        console.log(empId);
+        try {
+            await axios.delete(`https://springboot-homework.herokuapp.com/employees/${empId}`);
+            toast.success("Employee Deleted");
+            setLoad(!load);
+        } catch (error) {
+            toast.error("Employee cannot Deleted");
+        }
+    };
 
-  return (
-    <section className="get-employee grid">
-      <div className="get-employee-conatiner">
-        <div className="getemployee-button">
-          <button className="btn-get-employee" onClick={handleGetEmployees}>
-            Get All Employee
-          </button>
-          <button className="btn-get-employee" onClick={() => setToggle(false)}>
-            Get One Employee
-          </button>
-        </div>
-        {toggle ? (
-          <ul className="get-employees-list">
-            <li className="get-employee-item">
+    useEffect(() => {
+        handleGetEmployees();
+    }, [load, update]);
 
-              <p className="employee-info">ID</p>
-              <p className="employee-info email">Emp Email</p>
-              <p className="employee-info">FName</p>
-              <p className="employee-info">LName</p>
-              <p className="employee-info">Gender</p>
-              <p className="employee-info">Phone#</p>
-              <p className="employee-info">Modify</p>
-              <p className="employee-info">Delete</p>
-            </li>
-            {employees.map((employee, index) => {
-              const {
-                empId,
-                empEmail,
-                empFirstName,
-                empLastName,
-                empGender,
-                empPhoneNumber,
-              } = employee;
-              return (
-                <li key={index} className="get-employee-item">
-                  <p className="employee-info">{empId}</p>
-                  <p className="employee-info email">{empEmail}</p>
-                  <p className="employee-info">{empFirstName}</p>
-                  <p className="employee-info">{empLastName}</p>
-                  <p className="employee-info">{empGender}</p>
-                  <p className="employee-info phone">{empPhoneNumber}</p>
-                  <p className="employee-info">
-                    <button onClick={() => handleModify(empId)} className="btn-modify"> Modify</button>
-                  </p>
-                  <p className="employee-info">
-                    <button onClick={() => handleDelete(empId)} className="btn-delete"> Delete</button>
-                  </p>
-                </li>
-              );
-            })}
-          </ul>
-        ) : (
-          <GetOneEmployee />
-        )}
-      </div>
-      {modify ?  <ModifyEmployee data = {{setModify,employeeId,update,setUpdate}}/> : ""}
-    </section>
-  );
+    return (
+        <section className="get-employee grid">
+            <div className="get-employee-conatiner">
+                <div className="getemployee-button">
+                    <button className="btn-get-employee" onClick={handleGetEmployees}>
+                        Get All Employee
+                    </button>
+                    <button className="btn-get-employee" onClick={() => setToggle(false)}>
+                        Get One Employee
+                    </button>
+                </div>
+                {toggle ? (
+                    <ul className="get-employees-list">
+                        <li className="get-employee-item">
+
+                            <p className="employee-info">ID</p>
+                            <p className="employee-info email">Emp Email</p>
+                            <p className="employee-info">FName</p>
+                            <p className="employee-info">LName</p>
+                            <p className="employee-info">Gender</p>
+                            <p className="employee-info">Phone#</p>
+                            <p className="employee-info">Modify</p>
+                            <p className="employee-info">Delete</p>
+                        </li>
+                        {employees.map((employee, index) => {
+                            const {
+                                empId,
+                                empEmail,
+                                empFirstName,
+                                empLastName,
+                                empGender,
+                                empPhoneNumber,
+                            } = employee;
+                            return (
+                                <li key={index} className="get-employee-item">
+                                    <p className="employee-info">{empId}</p>
+                                    <p className="employee-info email">{empEmail}</p>
+                                    <p className="employee-info">{empFirstName}</p>
+                                    <p className="employee-info">{empLastName}</p>
+                                    <p className="employee-info">{empGender}</p>
+                                    <p className="employee-info phone">{empPhoneNumber}</p>
+                                    <p className="employee-info">
+                                        <button onClick={() => handleModify(empId)} className="btn-modify"> Modify
+                                        </button>
+                                    </p>
+                                    <p className="employee-info">
+                                        <button onClick={() => handleDelete(empId)} className="btn-delete"> Delete
+                                        </button>
+                                    </p>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                ) : (
+                    <GetOneEmployee/>
+                )}
+            </div>
+            {modify ? <ModifyEmployee data={{setModify, employeeId, update, setUpdate}}/> : ""}
+        </section>
+    );
 };
 
 export default GetEmployee;
--- src/component/getEmployee/GetOneEmployee.js
+++ src/component/getEmployee/GetOneEmployee.js
@@ -1,49 +1,50 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, {useState} from "react";
 import toast from "react-hot-toast";
 import {FaUserTie} from 'react-icons/fa'
 
 const GetOneEmployee = () => {
-  const [employee, setEmployee] = useState({});
-  const [showInfo,setShowInfo] = useState(false)
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const id = e.target.empId.value;
-    try {
-      const res = await axios.get(`http://localhost:8080/employees/${id}`);
-      setEmployee(res.data);
-      setShowInfo(true)
-      toast.success("Get Employee Info Success");
-    } catch (error) {
-      toast.error("Cannot connect Back End");
-    }
-  };
-  return (
-    <div className="get-employee-one">
-      <div className="title">Fill In Employee ID</div>
-      <form className="adding-one-form" onSubmit={handleSubmit}>
-        <label>
-          <input
-            type="text"
-            name="empId"
-            className="adding-one-input"
-            required
-            placeholder="Employee ID"
-          />
-        </label>
-        <button className="btn-get-one-employee">Submit</button>
-      </form>
-      {showInfo?
-      <div className="employee-info">
-        <FaUserTie className="usertie"/>
-        <p className="employee-info-one">First Name: {employee.empFirstName}</p>
-        <p className="employee-info-one">Last Name: {employee.empLastName}</p>
-        <p className="employee-info-one">Email: {employee.empEmail}</p>
-        <p className="employee-info-one">Gender: {employee.empGender}</p>
-        <p className="employee-info-one">Phone Number: {employee.empPhoneNumber}</p>
-      </div> : ""}
-    </div>
-  );
+    const [employee, setEmployee] = useState({});
+    const [showInfo, setShowInfo] = useState(false)
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const id = e.target.empId.value;
+        try {
+            const res = await axios.get(`http://localhost:8080/employees/${id}`);
+            setEmployee(res.data);
+            setShowInfo(true)
+            toast.success("Get Employee Info Success");
+        } catch (error) {
+            toast.error("Cannot connect Back End");
+        }
+    };
+    return (
+        <div className="get-employee-one">
+            <div className="title">Fill In Employee ID</div>
+            <form className="adding-one-form" onSubmit={handleSubmit}>
+                <label>
+                    <input
+                        type="text"
+                        name="empId"
+                        className="adding-one-input"
+                        required
+                        placeholder="Employee ID"
+                    />
+                </label>
+                <button className="btn-get-one-employee">Submit</button>
+            </form>
+            {showInfo ?
+                <div className="employee-info">
+                    <FaUserTie className="usertie"/>
+                    <p className="employee-info-one">First Name: {employee.empFirstName}</p>
+                    <p className="employee-info-one">Last Name: {employee.empLastName}</p>
+                    <p className="employee-info-one">Email: {employee.empEmail}</p>
+                    <p className="employee-info-one">Email: {employee.empEmail}</p>
+                    <p className="employee-info-one">Gender: {employee.empGender}</p>
+                    <p className="employee-info-one">Phone Number: {employee.empPhoneNumber}</p>
+                </div> : ""}
+        </div>
+    );
 };
 
 export default GetOneEmployee;
