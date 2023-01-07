537f01c97c848a6b571f3bbcaeb4165bf8f52eae
ttran65 <thang.tran65@t-mobile.com>
2023-01-07 13:41:08 -0600

    Thang

--- src/component/addEmployee/AddEmployee.js
+++ src/component/addEmployee/AddEmployee.js
@@ -1,80 +1,87 @@
-import React, {useRef} from "react";
+import React, { useRef } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
 import "./addEmployee.css";
-import {FaUserPlus} from 'react-icons/fa';
-
+import { FaUserPlus } from "react-icons/fa";
 
 const AddEmployee = () => {
-    const emailRef = useRef(null)
+  const emailRef = useRef(null);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const employee = {
-            empEmail: e.target.empEmail.value,
-            empFirstName: e.target.empFirstName.value,
-            empLastName: e.target.empLastName.value,
-            empGender: e.target.empGender.value,
-            empPhoneNumber: e.target.empPhoneNumber.value,
-        };
-        console.log(employee);
-
-
-        try {
-            const res = await axios.post("https://springboot-homework.herokuapp.com/employees", employee);
-            console.log(res.data)
-            toast.success("Employee create successfully");
-            e.target.reset()
-            emailRef.current.focus()
-
-        } catch (err) {
-            console.log("err");
-            toast.error("Employee create failed");
-        }
-
-
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const employee = {
+      empEmail: e.target.empEmail.value,
+      empFirstName: e.target.empFirstName.value,
+      empLastName: e.target.empLastName.value,
+      empGender: e.target.empGender.value,
+      empPhoneNumber: e.target.empPhoneNumber.value,
     };
-    return (
-        <section className="addemployee grid">
-            <div className="addemp-container">
-                <div className="add-title">
-                    <p>Adding New Employee</p>
-                    <div className="avatar">
-                        <FaUserPlus/>
-                    </div>
-                </div>
-                <form onSubmit={handleSubmit} className="adding-form submit">
-                    <label className="label">
-                        Email
-                        <input className="input-form-add" name="empEmail" type="email" ref={emailRef}/>
-                    </label>
-                    <label className="label">
-                        First Name
-                        <input className="input-form-add" name="empFirstName" type="text"/>
-                    </label>
+    console.log(employee);
 
-                    <label className="label">
-                        Last Name
-                        <input className="input-form-add" name="empLastName" type="text"/>
-                    </label>
+    try {
+      const res = await axios.post(
+        "https://springboot-employees.herokuapp.com/employees",
+        employee
+      );
+      console.log(res.data);
+      toast.success("Employee create successfully");
+      e.target.reset();
+      emailRef.current.focus();
+    } catch (err) {
+      console.log("err");
+      toast.error("Employee create failed");
+    }
+  };
+  return (
+    <section className="addemployee grid">
+      <div className="addemp-container">
+        <div className="add-title">
+          <p>Adding New Employee</p>
+          <div className="avatar">
+            <FaUserPlus />
+          </div>
+        </div>
+        <form onSubmit={handleSubmit} className="adding-form submit">
+          <label className="label">
+            Email
+            <input
+              className="input-form-add"
+              name="empEmail"
+              type="email"
+              ref={emailRef}
+            />
+          </label>
+          <label className="label">
+            First Name
+            <input className="input-form-add" name="empFirstName" type="text" />
+          </label>
 
-                    <label className="label">
-                        Gender
-                        <input className="input-form-add" name="empGender" type="text"/>
-                    </label>
-                    <label className="label">
-                        Phone Number
-                        <input className="input-form-add" name="empPhoneNumber" type="text"/>
-                    </label>
+          <label className="label">
+            Last Name
+            <input className="input-form-add" name="empLastName" type="text" />
+          </label>
 
-                    <button type="submit" className="submit-button">
-                        {" "}
-                        Adding New Employee
-                    </button>
-                </form>
-            </div>
-        </section>
-    );
+          <label className="label">
+            Gender
+            <input className="input-form-add" name="empGender" type="text" />
+          </label>
+          <label className="label">
+            Phone Number
+            <input
+              className="input-form-add"
+              name="empPhoneNumber"
+              type="text"
+            />
+          </label>
+
+          <button type="submit" className="submit-button">
+            {" "}
+            Adding New Employee
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default AddEmployee;
--- src/component/getEmployee/GetEmployee.js
+++ src/component/getEmployee/GetEmployee.js
@@ -1,107 +1,125 @@
 import axios from "axios";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import "./getemployee.css";
 import GetOneEmployee from "./GetOneEmployee";
 import ModifyEmployee from "../modifyEmployee/ModifyEmployee";
 
 const GetEmployee = () => {
-    const [load, setLoad] = useState(false);
-    const [toggle, setToggle] = useState(true);
-    const [employees, setEmployees] = useState([]);
-    const [modify, setModify] = useState(false)
-    const [update, setUpdate] = useState(false)
-    const [employeeId, setEmployeeId] = useState("")
-    const handleModify = (empId) => {
-        setModify(true)
-        setEmployeeId(empId)
+  const [load, setLoad] = useState(false);
+  const [toggle, setToggle] = useState(true);
+  const [employees, setEmployees] = useState([]);
+  const [modify, setModify] = useState(false);
+  const [update, setUpdate] = useState(false);
+  const [employeeId, setEmployeeId] = useState("");
+  const handleModify = (empId) => {
+    setModify(true);
+    setEmployeeId(empId);
+  };
+
+  const handleGetEmployees = async () => {
+    try {
+      const res = await axios.get(
+        "https://springboot-employees.herokuapp.com/employees/all"
+      );
+      setEmployees(res.data);
+      toast.success("Get Employees Completed");
+    } catch (error) {
+      toast.error("Cannot coonect DB");
     }
+    setToggle(true);
+  };
 
-    const handleGetEmployees = async () => {
-        try {
-            const res = await axios.get("https://springboot-homework.herokuapp.com/employees/all");
-            setEmployees(res.data);
-            toast.success("Get Employees Completed");
-        } catch (error) {
-            toast.error("Cannot coonect DB");
-        }
-        setToggle(true);
-    };
+  const handleDelete = async (empId) => {
+    console.log(empId);
+    try {
+      await axios.delete(
+        `https://springboot-employees.herokuapp.com/employees/${empId}`
+      );
+      toast.success("Employee Deleted");
+      setLoad(!load);
+    } catch (error) {
+      toast.error("Employee cannot Deleted");
+    }
+  };
 
-    const handleDelete = async (empId) => {
-        console.log(empId);
-        try {
-            await axios.delete(`https://springboot-homework.herokuapp.com/employees/${empId}`);
-            toast.success("Employee Deleted");
-            setLoad(!load);
-        } catch (error) {
-            toast.error("Employee cannot Deleted");
-        }
-    };
+  useEffect(() => {
+    handleGetEmployees();
+  }, [load, update]);
 
-    useEffect(() => {
-        handleGetEmployees();
-    }, [load, update]);
-
-    return (
-        <section className="get-employee grid">
-            <div className="get-employee-conatiner">
-                <div className="getemployee-button">
-                    <button className="btn-get-employee" onClick={handleGetEmployees}>
-                        Get All Employee
+  return (
+    <section className="get-employee grid">
+      <div className="get-employee-conatiner">
+        <div className="getemployee-button">
+          <button className="btn-get-employee" onClick={handleGetEmployees}>
+            Get All Employee
+          </button>
+          <button className="btn-get-employee" onClick={() => setToggle(false)}>
+            Get One Employee
+          </button>
+        </div>
+        {toggle ? (
+          <ul className="get-employees-list">
+            <li className="get-employee-item">
+              <p className="employee-infos">ID</p>
+              <p className="employee-infos email">Emp Email</p>
+              <p className="employee-infos">FName</p>
+              <p className="employee-infos">LName</p>
+              <p className="employee-infos">Gender</p>
+              <p className="employee-infos phone">Phone#</p>
+              <p className="employee-infos">Modify</p>
+              <p className="employee-infos">Delete</p>
+            </li>
+            {employees.map((employee, index) => {
+              const {
+                empId,
+                empEmail,
+                empFirstName,
+                empLastName,
+                empGender,
+                empPhoneNumber,
+              } = employee;
+              return (
+                <li key={index} className="get-employee-item">
+                  <p className="employee-infos">{empId}</p>
+                  <p className="employee-infos email">{empEmail}</p>
+                  <p className="employee-infos">{empFirstName}</p>
+                  <p className="employee-infos">{empLastName}</p>
+                  <p className="employee-infos">{empGender}</p>
+                  <p className="employee-infos phone">{empPhoneNumber}</p>
+                  <p className="employee-infos">
+                    <button
+                      onClick={() => handleModify(empId)}
+                      className="btn-modify"
+                    >
+                      {" "}
+                      Modify
                     </button>
-                    <button className="btn-get-employee" onClick={() => setToggle(false)}>
-                        Get One Employee
+                  </p>
+                  <p className="employee-infos">
+                    <button
+                      onClick={() => handleDelete(empId)}
+                      className="btn-delete"
+                    >
+                      {" "}
+                      Delete
                     </button>
-                </div>
-                {toggle ? (
-                    <ul className="get-employees-list">
-                        <li className="get-employee-item">
-                            <p className="employee-infos">ID</p>
-                            <p className="employee-infos email">Emp Email</p>
-                            <p className="employee-infos">FName</p>
-                            <p className="employee-infos">LName</p>
-                            <p className="employee-infos">Gender</p>
-                            <p className="employee-infos phone">Phone#</p>
-                            <p className="employee-infos">Modify</p>
-                            <p className="employee-infos">Delete</p>
-                        </li>
-                        {employees.map((employee, index) => {
-                            const {
-                                empId,
-                                empEmail,
-                                empFirstName,
-                                empLastName,
-                                empGender,
-                                empPhoneNumber,
-                            } = employee;
-                            return (
-                                <li key={index} className="get-employee-item">
-                                    <p className="employee-infos">{empId}</p>
-                                    <p className="employee-infos email">{empEmail}</p>
-                                    <p className="employee-infos">{empFirstName}</p>
-                                    <p className="employee-infos">{empLastName}</p>
-                                    <p className="employee-infos">{empGender}</p>
-                                    <p className="employee-infos phone">{empPhoneNumber}</p>
-                                    <p className="employee-infos">
-                                        <button onClick={() => handleModify(empId)} className="btn-modify"> Modify
-                                        </button>
-                                    </p>
-                                    <p className="employee-infos">
-                                        <button onClick={() => handleDelete(empId)} className="btn-delete"> Delete
-                                        </button>
-                                    </p>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                ) : (
-                    <GetOneEmployee/>
-                )}
-            </div>
-            {modify ? <ModifyEmployee data={{setModify, employeeId, update, setUpdate}}/> : ""}
-        </section>
-    );
+                  </p>
+                </li>
+              );
+            })}
+          </ul>
+        ) : (
+          <GetOneEmployee />
+        )}
+      </div>
+      {modify ? (
+        <ModifyEmployee data={{ setModify, employeeId, update, setUpdate }} />
+      ) : (
+        ""
+      )}
+    </section>
+  );
 };
 
 export default GetEmployee;
